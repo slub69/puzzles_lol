e8417337ee81011cb8638ed1426cfccfd1024a63
slub69 <slublins2@gmail.com>
2023-11-01 10:15:55 -0400

    11/1/23

---
+++ java.js
@@ -0,0 +1,17 @@
+const playlist = ["A", "B", "C", "D", "E"]
+
+function button1(playlist){
+
+}
+function button2(playlist){
+
+}
+function button3(playlist){
+
+}
+function button4(playlist){
+
+}
+function screen(){
+    if 
+}
