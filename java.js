90de5c62516d9d66a4689b531bbb3a2cd1b47cbd
slub69 <slublins2@gmail.com>
2023-11-02 09:34:06 -0400

    11/2/23

--- java.js
+++ java.js
@@ -1,17 +1,22 @@
 const playlist = ["A", "B", "C", "D", "E"]
-
-function button1(playlist){
-
+function screen(b,n,playlist){
+    for(i=0;i<=n;i++){
+        if(b==='1'){ //button 1
+            playlist.push(playlist[0])
+            playlist.slice(1)
+        } else if(b==='2'){ //button 2
+            playlist.unshift(playlist[playlist.length -1])
+            playlist.pop()
+        } else if(b==='3'){ //button 3
+            let first = playlist[0]
+            playlist[0] = playlist[1]
+            playlist[1] = first
+        } else if(b==='4'){ //button 4
+            break
+        }
+    }
 }
-function button2(playlist){
-
-}
-function button3(playlist){
-
-}
-function button4(playlist){
-
-}
-function screen(){
-    if 
+while(1){
+    screen(/*queary input for b*/,/*queary input for n*/,playlist)
+    return playlist
 }
