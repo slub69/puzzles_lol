b49603179896d843e5621f160822c30de41d2671
slub69 <slublins2@gmail.com>
2023-11-03 10:15:08 -0400

    11/3/23

---
+++ towers_of_hanoi.js
@@ -0,0 +1,19 @@
+const TowerOfHanoi = [1,2,3,4] //represents intial tower
+let c1 = TowerOfHanoi //column 1
+let c2 = [] //column 2
+let c3 = [] //column 3
+function mover(c1,c2,c3){
+    if(c3===TowerOfHanoi){ //if tower is sorted then break and return sorted
+        return console.log('tower has been shift:\n'+c1+'\n'+c2+'\n'+c3)
+    }else{} //if not then lets get to work
+    c2.push(c1[0]) //move first disk to second column
+    c1.shift(c2.length)//remove that disk from the original
+    c3.push(c1[0])//move second disk to third column
+    c1.shift(c3.length)//remove taht disk from the orginal
+    console.log('column 1: ' +c1)
+    console.log('column 2: ' +c2)
+    console.log('column 3: ' +c3)
+    //return mover(c1,c2,c3)
+    
+}
+mover(c1,c2,c3)
